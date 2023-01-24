03f925a490bd7dc6f154ffff764f4e3d563404aa
Devin Cunningham <dtyler.cunningham@gmail.com>
2023-01-24 15:51:58 -0800

    css

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -3,13 +3,15 @@ import { PostMetadata } from './PostMetadata';
 
 const PostPreview = (props: PostMetadata) => {
   return (
-    <div className="border border-slate-300 p-4 rounded-md shadow-sm bg-white">
+    <Link
+      href={`/posts/${props.slug}`}
+      className="border border-slate-300 p-4 rounded-md shadow-sm bg-white group transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-102 hover:shadow-lg hover:rounded-xl hover:transition-all duration-200"
+    >
+      <h2 className="text-green-600">{props.title}</h2>
       <p className="text-sm text-slate-400">{props.date}</p>
-      <Link href={`/posts/${props.slug}`}>
-        <h2 className="text-green-600 hover:underline mb-4">{props.title}</h2>
-      </Link>
-      <p className="text-slate-700">{props.subtitle}</p>
-    </div>
+      <hr className="mb-4" />
+      <p className="text-slate-700 text-sm">{props.subtitle}</p>
+    </Link>
   );
 };
 
