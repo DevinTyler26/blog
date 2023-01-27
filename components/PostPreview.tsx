c57cea0496edfda90f38c51b41d9f7f4cbdd25b9
Devin Cunningham <dtyler.cunningham@gmail.com>
2023-01-27 12:40:03 -0800

    smaller images

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -14,11 +14,11 @@ const PostPreview = (props: PostMetadata) => {
       href={`/posts/${props.slug}`}
       className="border border-slate-300 rounded-md shadow-sm bg-white group transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-102 hover:shadow-lg hover:rounded-xl hover:transition-all duration-200 overflow-hidden"
     >
-      <Image src={imagePath} alt={`hero image ${props.slug}`} width={700} height={500} className=" object-cover" />
+      <Image src={imagePath} alt={`hero image ${props.slug}`} width={700} height={400} className="object-cover" blurDataURL="/images/hero/no-image.png" placeholder="blur" />
       <div className="p-4">
         <h2 className="text-green-600">{props.title}</h2>
-        <p className="text-sm text-slate-400">{props.date}</p>
-        <hr className="mb-4" />
+        <hr className="" />
+        <p className="text-slate-400 text-xs mb-4">{props.date}</p>
         <p className="text-slate-700 text-sm">{props.subtitle}</p>
       </div>
     </Link>
