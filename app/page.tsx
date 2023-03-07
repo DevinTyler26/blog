e81101516e9c10d2a48b6f8feea4538299171e38
Devin Cunningham <dtyler.cunningham@gmail.com>
2023-03-07 11:08:05 -0800

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
+import getPostMetadata from '@/components/getPostMetadata';
 import { PostMetadata } from '@/components/PostMetadata';
 import PostPreview from '@/components/PostPreview';
 
-async function getData(): Promise<{ posts: PostMetadata[] }> {
-  const res = await fetch(`${process.env.NEXT_URL}/api/posts`, { next: { revalidate: 60 * 60 } });
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-
-  return res.json();
+function getData(): PostMetadata[] {
+  const postMetadata = getPostMetadata();
+  const currentDate = new Date();
+  const timezoneOffset = currentDate.getTimezoneOffset();
+  currentDate.setMinutes(currentDate.getMinutes() - timezoneOffset);
+  const posts = postMetadata
+    .filter((obj) => {
+      const objDate = new Date(obj.date);
+      return objDate <= currentDate;
+    })
+    .sort((a, b) => new Date(b.date).valueOf() - new Date(a.date).valueOf());
+  return posts;
 }
 
 const HomePage = async () => {
-  const { posts } = await getData();
+  const posts = getData();
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
